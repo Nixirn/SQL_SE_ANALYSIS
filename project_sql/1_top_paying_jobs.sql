d303a9f3deabc75123effcc2e48b1436cd5efe70
Erick Lagunas <70537048+Nixirn@users.noreply.github.com>
2025-07-31 11:00:14 -0700

    Added top 10 salaried job postings for software engineer.

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,21 @@
+/*
+    Question: What were the top-paying Software Engineer jobs in 2023?
+    1. Identify the top 10 Software Engineering roles available remotely at the time
+        a. Focus on postings with listed salaries
+    Why? This will give a picture of how different the job market was a few years ago 
+*/
+
+SELECT 
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM 
+    job_postings_fact
+WHERE
+    job_title_short = 'Software Engineer' AND 
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+LIMIT 10;
